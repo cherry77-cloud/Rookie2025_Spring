c02f7d1526f920a0ac70d2726cc174123d2953c0
cherry77-cloud <cherry1615405@gmail.com>
2025-06-02 17:30:09 +0800

    Update 5-7_client_oob.c

--- chapter_05/5-7_client_oob.c
+++ chapter_05/5-7_client_oob.c
@@ -25,7 +25,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
     if (argc <= 2) {
         printf("usage: %s ip_address port_number\n", basename(argv[0]));
         return 1;
