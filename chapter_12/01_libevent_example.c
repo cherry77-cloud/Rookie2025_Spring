7f40cd9fc76d270afcf15953b9806fd6fbb77aaf
cherry77-cloud <cherry1615405@gmail.com>
2025-06-06 18:48:16 +0800

    Update 01_libevent_example.c

--- chapter_12/01_libevent_example.c
+++ chapter_12/01_libevent_example.c
@@ -1,25 +1,25 @@
-#include <sys/signal.h>
 #include <event.h>
+#include <stdio.h>
+#include <sys/signal.h>
+#include <sys/time.h>
 
-void signal_cb(int fd, short event, void* argc) {
-    struct event_base* base = (event_base*)argc;
-    struct timeval delay = { 2, 0 };
+void signal_cb(int fd, short event, void *argc) {
+    struct event_base *base = (struct event_base *)argc;
+    struct timeval delay = {2, 0};
     printf("Caught an interrupt signal; exiting cleanly in two seconds...\n");
     event_base_loopexit(base, &delay);
 }
 
-void timeout_cb(int fd, short event, void* argc) {
-    printf("timeout\n");
-}
+void timeout_cb(int fd, short event, void *argc) { printf("timeout\n"); }
 
 int main() {
-    struct event_base* base = event_init();
+    struct event_base *base = event_init();
 
-    struct event* signal_event = evsignal_new(base, SIGINT, signal_cb, base);
+    struct event *signal_event = evsignal_new(base, SIGINT, signal_cb, base);
     event_add(signal_event, NULL);
 
-    timeval tv = { 1, 0 };
-    struct event* timeout_event = evtimer_new(base, timeout_cb, NULL);
+    struct timeval tv = {1, 0};
+    struct event *timeout_event = evtimer_new(base, timeout_cb, NULL);
     event_add(timeout_event, &tv);
 
     event_base_dispatch(base);
